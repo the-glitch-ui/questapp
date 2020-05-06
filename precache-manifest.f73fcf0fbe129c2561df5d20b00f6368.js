3b3cbcebb3c9e67655b14d7317ca4b895ea7ddeb
Work <work@SRs-iMac.local>
2020-05-06 11:49:39 +0200

    Updates

---
+++ precache-manifest.f73fcf0fbe129c2561df5d20b00f6368.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48cc4350d467c94b4142235f24a7080",
+    "url": "/questapp/index.html"
+  },
+  {
+    "revision": "bcfdaef3466abb3e26a9",
+    "url": "/questapp/static/css/main.072b94c7.chunk.css"
+  },
+  {
+    "revision": "21e9929fae9880b3a02e",
+    "url": "/questapp/static/js/2.49a1e95a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/questapp/static/js/2.49a1e95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcfdaef3466abb3e26a9",
+    "url": "/questapp/static/js/main.16e9a7c0.chunk.js"
+  },
+  {
+    "revision": "350de6ae95059bea9c5a",
+    "url": "/questapp/static/js/runtime-main.b8ed0df6.js"
+  },
+  {
+    "revision": "dc3d9960bc52c80bec92253cb07150b6",
+    "url": "/questapp/static/media/background.dc3d9960.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/questapp/precache-manifest.f73fcf0fbe129c2561df5d20b00f6368.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/questapp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
